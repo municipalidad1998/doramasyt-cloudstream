4d24397974d2c73e18a7741a9aa741558b779aae
municipalidad1998 <125228570+municipalidad1998@users.noreply.github.com>
2026-09-13 17:06:04 -0600

    Add Nuvio DoramaYT source structure

---
+++ src/doramasyt/index.js
@@ -0,0 +1,17 @@
+import { getTmdbTitle, searchDorama, getEpisodeUrl } from "./search.js";
+import { extractStreams } from "./extract.js";
+
+export async function getStreams(tmdbId, mediaType, season, episode) {
+  try {
+    const title = await getTmdbTitle(tmdbId, mediaType);
+    const detail = await searchDorama(title);
+    const pageUrl = mediaType === "tv" && episode ? await getEpisodeUrl(detail, episode) : detail;
+    const streams = await extractStreams(pageUrl);
+    return streams;
+  } catch (error) {
+    console.error("[DoramaYT] " + error.message);
+    return [];
+  }
+}
+
+module.exports = { getStreams };
